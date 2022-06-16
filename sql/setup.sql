74107a254e6382403127aa25b18bf916675f1cab
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-16 14:14:31 -0700

    in setup.sql file added data for table movies that includes id, name of movie, director, and genre

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE if exists movies
+
+CREATE TABLE movies (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    director VARCHAR NOT NULL,
+    genre VARCHAR NOT NULL
+);
+
+INSERT INTO movies (name, director, genre) VALUES
+('Blade Runner', 'Ridley Scott', 'Science Fiction'),
+('Fight Club', 'David Fincher', 'psychological action drama'),
+('Remember the Titans', 'Boaz Yakin', 'Sports')
