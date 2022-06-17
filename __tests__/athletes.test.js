7ea10005c38462aa4550fce5c9fa92076dbe958c
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-17 11:18:02 -0700

    wrote slice for second operation, /athletes/:id

--- __tests__/athletes.test.js
+++ __tests__/athletes.test.js
@@ -13,4 +13,15 @@ describe('athlete routes', () => {
     expect(resp.status).toEqual(200);
     expect(resp.body.length).toEqual(3);
   });
+
+  it('/athletes/:id should return the athlete detail', async () => {
+    const resp = await request(app).get('/athletes/1');
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual({
+      id: '1',
+      name: 'Ricky Lightfoot',
+      team: 'Wales',
+      sport: 'man vs horse racing',
+    });
+  });
 });
