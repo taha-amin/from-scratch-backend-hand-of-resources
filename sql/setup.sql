2bee69119584910faa43c9cf36402b4fd7e1a3c4
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-17 10:44:34 -0700

    created table for second resource

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE if exists movies;
+DROP TABLE if exists athletes;
 
 CREATE TABLE movies (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -13,3 +14,10 @@ INSERT INTO movies (name, director, genre) VALUES
 ('Blade Runner', 'Ridley Scott', 'Science Fiction'),
 ('Fight Club', 'David Fincher', 'psychological action drama'),
 ('Remember the Titans', 'Boaz Yakin', 'Sports')
+
+CREATE TABLE athletes (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    team VARCHAR NOT NULL,
+    sport VARCHAR NOT NULL
+);
