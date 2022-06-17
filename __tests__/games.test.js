411e2770327cb69495b57357dfb740118caec15a
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-17 12:57:45 -0700

    wrote slice for second operation, /games/:id

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -13,4 +13,15 @@ describe('game routes', () => {
     expect(resp.status).toEqual(200);
     expect(resp.body.length).toEqual(3);
   });
+
+  it('/games/:id should return the game detail', async () => {
+    const resp = await request(app).get('/games/1');
+    expect(resp.status).toEqual(200);
+    expect(resp.body).toEqual({
+      id: '1',
+      name: 'tic tac toe',
+      players: 2,
+      genre: 'paper and pencil',
+    });
+  });
 });
